9325b8470d5ec590a18e210e2e6bcd39e3a9ae2d
ays <ays@teste.br>
2024-09-25 22:01:31 -0300

    criacao

---
+++ src/main/java/ays/dio/CursoSpringNuvemApplication.java
@@ -0,0 +1,13 @@
+package ays.dio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursoSpringNuvemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursoSpringNuvemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ays/dio/CursoSpringNuvemApplicationTests.java
@@ -0,0 +1,13 @@
+package ays.dio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursoSpringNuvemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
